fc90534d55efff5166d3e0d671a390f2595abe23
bharath2904 <bharathwaj2904@gmail.com>
2024-08-28 00:08:18 +0530

    The updated of project

--- src/components/home.js
+++ src/components/home.js
@@ -45,6 +45,12 @@ function Home() {
               </li>
               <li className="line"></li>
               <li className="nav-item">
+                <a className="nav-link me-5 text-light" href="#certificate">
+                  Certifications
+                </a>
+              </li>
+              <li className="line"></li>
+              <li className="nav-item">
                 <a className="nav-link me-5 text-light" href="#contact">
                   Contact
                 </a>
@@ -53,7 +59,7 @@ function Home() {
           </div>
         </nav>
 
-        <div className="firs">
+        <div className="first">
           <div className="container-lg bg-dark-subtle p-5 my-5 rounded phill border border-2">
             <h1 className="text-center text-muted  fw-bolder">
               I'M BHARATHWAJ
@@ -365,8 +371,8 @@ function Home() {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
-                height="50px"
-                width="50px"
+                height="40px"
+                width="40px"
                 fill="#000000"
               >
                 <path d="M184 48l144 0c4.4 0 8 3.6 8 8l0 40L176 96l0-40c0-4.4 3.6-8 8-8zm-56 8l0 40L64 96C28.7 96 0 124.7 0 160l0 96 192 0 128 0 192 0 0-96c0-35.3-28.7-64-64-64l-64 0 0-40c0-30.9-25.1-56-56-56L184 0c-30.9 0-56 25.1-56 56zM512 288l-192 0 0 32c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32-14.3-32-32l0-32L0 288 0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-128z" />
@@ -483,6 +489,34 @@ function Home() {
         {/* Project section is end */}
       </section>
 
+      {/* Certificate section is start */}
+      <section id="certificate">
+        
+          <h4 className="text-start mb-4 mt-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="50px" height="50px" fill="#000000"><path d="M211 7.3C205 1 196-1.4 187.6 .8s-14.9 8.9-17.1 17.3L154.7 80.6l-62-17.5c-8.4-2.4-17.4 0-23.5 6.1s-8.5 15.1-6.1 23.5l17.5 62L18.1 170.6c-8.4 2.1-15 8.7-17.3 17.1S1 205 7.3 211l46.2 45L7.3 301C1 307-1.4 316 .8 324.4s8.9 14.9 17.3 17.1l62.5 15.8-17.5 62c-2.4 8.4 0 17.4 6.1 23.5s15.1 8.5 23.5 6.1l62-17.5 15.8 62.5c2.1 8.4 8.7 15 17.1 17.3s17.3-.2 23.4-6.4l45-46.2 45 46.2c6.1 6.2 15 8.7 23.4 6.4s14.9-8.9 17.1-17.3l15.8-62.5 62 17.5c8.4 2.4 17.4 0 23.5-6.1s8.5-15.1 6.1-23.5l-17.5-62 62.5-15.8c8.4-2.1 15-8.7 17.3-17.1s-.2-17.4-6.4-23.4l-46.2-45 46.2-45c6.2-6.1 8.7-15 6.4-23.4s-8.9-14.9-17.3-17.1l-62.5-15.8 17.5-62c2.4-8.4 0-17.4-6.1-23.5s-15.1-8.5-23.5-6.1l-62 17.5L341.4 18.1c-2.1-8.4-8.7-15-17.1-17.3S307 1 301 7.3L256 53.5 211 7.3z"/></svg> Certifications</h4>
+          <div className="certificate container-lg bg-dark-subtle p-3 border rounded pill">
+            <h4>The Complete SQL Bootcamp</h4>
+            <h5 className="ms-5 mt-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Issuing Organization&nbsp;&nbsp;&nbsp;: <span className="text-primary">Udemy</span> </h5>
+            <h5 className="ms-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Date Received&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Feb. 20, 2024</h5>
+            <h5 className="ms-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.51-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Certification Url&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <a className="text-decoration-none text-dark" href="https://www.demy.com/certificate/UC-a831f839-2360-4fcb-89e7-ff3387cb816c/"><span className="url">Certificate URL</span></a></h5>
+            <h5 className="ms-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Description&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <span className="lead text-success" style={{textAlign:"jusutify"}}> This certificate verifies the successful completion of an intensive SQL Bootcamp, covering foundational to advanced SQL concepts. The bootcamp provided comprehensive training in SQL for data management, querying, and manipulation.</span></h5>
+
+            <h4 className="mt-5">The SQL(Basics)</h4>
+            <h5 className="ms-5 mt-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Issuing Organization&nbsp;&nbsp;&nbsp;: <span className="text-primary">HackerRank</span> </h5>
+            <h5 className="ms-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Date Received&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Aug. 22, 2024</h5>
+            <h5 className="ms-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Certification Url&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <a className="text-decoration-none text-dark" href="https://www.hackerrank.com/certificates/5998748943ff"><span className="url">Certificate URL</span></a></h5>
+            <h5 className="ms-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Description&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <span className="lead text-success" style={{textAlign:"jusutify"}}> The SQL Basic Problem-Solving Certification from HackerRank demonstrates proficiency in foundational SQL skills through practical problem-solving. This certification reflects the successful completion of various challenges designed to test and validate fundamental SQL knowledge</span></h5>
+
+
+            <h4 className="mt-5">The SQL(Intermediate)</h4>
+            <h5 className="ms-5 mt-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Issuing Organization&nbsp;&nbsp;&nbsp;: <span className="text-primary">HackerRank</span> </h5>
+            <h5 className="ms-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Date Received&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Aug. 22, 2024</h5>
+            <h5 className="ms-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Certification Url&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <a className="text-decoration-none text-dark" href="https://www.hackerrank.com/certificates/f703dbd2b6c3"><span className="url">Certificate URL</span></a></h5>
+            <h5 className="ms-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40px" height="40px" fill="#000000"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg> Description&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <span className="lead text-success" style={{textAlign:"jusutify"}}> The SQL Intermediate Problem-Solving Certification from HackerRank signifies advanced SQL problem-solving skills through a series of intermediate-level challenges. This certification indicates the successful resolution of more complex SQL tasks and scenarios.</span></h5>
+        </div>
+      </section>
+      {/* certfications section is start */}
+
+      {/* contact section is start */}
       <section id="contact" className="py-4">
         <div className="container-lg">
           <h4 className="text-start mb-4">
